b3d0a2156121d1f26f2f2acacaf8f5041309ec04
yanelysvelarde <yanelys932@gmail.com>
2023-09-19 18:41:29 -0400

    123

---
+++ project.js
@@ -0,0 +1,19 @@
+// 1. desposit some money
+// 2. determine number of lines to bet
+// 3. collect a bet amount
+// 4. spin the slot machine
+//5. check if the user won
+// 6. give the user their winnings
+// 7. play again
+
+const prompt = require("prompt-sync")();  // calls package prpmt-sync
+
+//create a function/cosntant
+const deposit = () => {     //this is the same as                 function deposit() {    }
+
+    const depositAmount = prompt("Enter a deposit amount: ");
+
+
+};
+
+deposit();
