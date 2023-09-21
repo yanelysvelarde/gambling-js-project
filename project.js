07b5a917e0b09e2b0f0a3cd8954dff86e94adbc2
yanelysvelarde <yanelys932@gmail.com>
2023-09-21 00:07:26 -0400

    123

--- project.js
+++ project.js
@@ -139,19 +139,57 @@ console.log(rowString)
 }
 
 };
+const getWinnings = (rows,bet,lines) => {
+    let winnings = 0;
+    for (let row =0;row<lines;row++){
+        const symbols = rows[row];
+        let allSame = true;
+
+        for(const symbol of symbols){
+            if(symbol != symbols[0]){
+                allSame=false;
+                break;
+            }
+        }
+
+        if(allSame){
+            winnings +=bet*SYMBOL_VALUES[symbols[0]] 
+        }
+    }
+    return winnings;
+};
+
+const game = () => {
+    let balance = deposit(); //with a constant you cannot change the value, with let you can
+
+
+while(true){
+    console.log("You have a balance of $"+balance);
+    const numberOfLines = getNumberOfLines();
+    const bet = getBet(balance, numberOfLines);
+    balance -= bet * numberOfLines;
+    const reels = spin();
+    const rows = transpose(reels);
+    printRows(rows);
+    const winnings = getWinnings(rows,bet,numberOfLines);
+    balance += winnings;
+    console.log("You won, $" +winnings.toString());
+
+    if (balance <=0){
+        console.log("You ran out of money");
+        break;
+    }
+const playAgain = prompt("Do you want to play again (y/n)? ") ;
+if (playAgain != "y") break;
+
+
+}
+
+};
+
+game();
 
 
 
-
-
-
-
-let balance = deposit(); //with a constant you cannot change the value, with let you can
-const numberOfLines = getNumberOfLines();
-const bet =getBet(balance, numberOfLines);
-
-const reels = spin();
-const rows = transpose(reels);
-printRows(rows);
 
 
